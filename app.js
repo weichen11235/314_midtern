927f8a9be2b6e703c541b8aff24ae53532a55785
weichen11235 <44263103+weichen11235@users.noreply.github.com>
2020-03-11 16:19:07 -0400

    finish project

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const timeRunOut = document.getElementById('noTime');
 const taskInput = document.getElementById('taskName');
 const timeInput = document.getElementById('time');
 const required = document.getElementById('required');
@@ -14,16 +15,22 @@ function taskAdder(){
     required.style.display = "none";
     let taskItem = taskItemMaker(taskInput.value);
     taskList.appendChild(taskItem);
+    taskInput.value = "";
   } else if(taskInput.value !== "" && required.style.display != "none" && timeInput !== "") {
     required.style.display = "none";
     let taskItem = taskItemMaker(taskInput.value, Number(timeInput.value));
     taskList.appendChild(taskItem);
+    taskInput.value = "";
+    timeInput.value = "";
   } else if(timeInput.value === "" && taskInput !== "") {
     let taskItem = taskItemMaker(taskInput.value);
     taskList.appendChild(taskItem);
+    taskInput.value = "";
   } else {
     let taskItem = taskItemMaker(taskInput.value, Number(timeInput.value));
     taskList.appendChild(taskItem);
+    taskInput.value = "";
+    timeInput.value = "";
   }
 }
 
@@ -43,22 +50,51 @@ function taskItemMaker(taskValue, timeValue = "0") {
 
 function optionsMaker(timeValue) {
   if(timeValue === "0") {
-    let options = '<input type="number" name="" id="time2" placeholder="specify time in minute"><button id="addTime" onclick="timeAdder(document.getElementById("time2").value)">add time</button>'
+    let options = '<input type="number" name="" id="time2" placeholder="specify time in minute"><button id="addTime" onclick="timeAdder(Number(document.getElementById(\'time2\').value), this)">add time</button> <button class="delete" onclick="deleteTask(this)">delete</button>';
     return options;
   } else {
     let options = `<span class="timer">${timeValue}:00</span>
-    <button class="start" onclick="timeStart">start</button>
-    <button class="delete" conclick="deleteTask(e)">delete</button>`
+    <button class="start" onclick="timeStart(this)">start</button>
+    <button class="delete" onclick="deleteTask(this)">delete</button>`
     return options;
   }
 }
 
-function timeAdder(time2Value) {
-
+function timeAdder(time2Value, id) {
+  let taskItem = id.parentElement.parentElement;
+  let oldOptions = id.parentElement;
+  let newOptions = document.createElement('div');
+  newOptions.className = "options";
+  newOptions.innerHTML = optionsMaker(time2Value);
+  taskItem.replaceChild(newOptions, oldOptions);
 }
 
-function timeStart() {
-
+function timeStart(id) {
+  let timer = id.previousElementSibling;
+  let time = id.previousElementSibling.innerText;
+  let totalMinutes, totalSeconds;
+  if(time.length === 4) {
+    totalMinutes = Number(time.charAt(0));
+    totalSeconds = totalMinutes * 60;
+  } else {
+    totalMinutes = Number(time.slice(0, 2));
+    totalSeconds = totalMinutes * 60;
+  }
+  let countDown = setInterval(function() {
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    timer.innerText = `${minutes}:${seconds}`;
+    if(timer.innerText === "0:30"){
+      timeRunOut.style.display = "block"
+      setTimeout(function(){
+      timeRunOut.style.display = "none"
+      }, 5000)
+    } else if(timer.innerText === "0:00") {
+      clearInterval(countDown);
+    }
+    totalSeconds--;
+  }, 1000);
 }
 
 function deleteTask(id) {
