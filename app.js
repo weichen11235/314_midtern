921e30a750a2a76d822193c04ba6088615fffa1c
weichen11235 <44263103+weichen11235@users.noreply.github.com>
2020-03-10 21:25:53 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const taskInput = document.getElementById('taskName');
+const timeInput = document.getElementById('time');
+const addTask = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
+const clearBtn = document.getElementById('clear');
